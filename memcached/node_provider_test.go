0e25046f0582e6ae69ff6a911fefd479aa4f4b91
Yosh11 <isok.1997@mail.ru>
2024-01-31 18:27:15 +0300

    test add coverage test for node provider

--- memcached/node_provider_test.go
+++ memcached/node_provider_test.go
@@ -4,14 +4,18 @@ import (
 	"context"
 	"errors"
 	"net"
+	"slices"
 	"sync"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
 
+	"github.com/aliexpressru/gomemcached/consistenthash"
 	"github.com/aliexpressru/gomemcached/logger"
+	"github.com/aliexpressru/gomemcached/utils"
 )
 
 func Test_getNodes(t *testing.T) {
@@ -242,54 +246,191 @@ func Test_nodeIsDead(t *testing.T) {
 }
 
 func Test_initNodesProvider(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.TODO())
-	c := &Client{ctx: ctx}
-	c.initNodesProvider()
+	var (
+		mockNetworkErr = new(MockNetworkOperations)
 
+		period = 10 * time.Millisecond
+
+		ctx, cancel = context.WithCancel(context.TODO())
+		expectedErr = errors.New("mocked dial error")
+	)
+	cl := &Client{
+		ctx: ctx,
+		nw: &network{
+			dial:       mockNetworkErr.Dial,
+			lookupHost: mockNetworkErr.LookupHost,
+		},
+		cfg: &config{
+			HeadlessServiceAddress: "example.com",
+		},
+		nodeHCPeriod: period,
+		nodeRBPeriod: period,
+	}
+
+	mockNetworkErr.On("LookupHost", cl.cfg.HeadlessServiceAddress).Return(nil, expectedErr)
+	mockNetworkErr.On("Dial", mock.Anything, mock.Anything).Return(&FakeConn{}, nil)
+
+	cl.initNodesProvider()
+
+	mockNetworkErr.AssertNotCalled(t, "Dial")
+
+	count := 4
+	<-time.After(period * time.Duration(count))
 	cancel()
+
+	mockNetworkErr.AssertNumberOfCalls(t, "LookupHost", 2*count-2)
 }
 
 func Test_checkNodesHealth(t *testing.T) {
-	// addr := &net.TCPAddr{IP: net.ParseIP("127.0.0.1"), Port: 12345}
-	// expectedErr := errors.New("mocked dial error")
-	//
-	// var (
-	// 	currentNodes     = []string{"node1:12345", "node2", "node3:12345", "node4:12345", "node5:12345"}
-	// 	alreadyDeadNodes = []string{"node5:12345"}
-	// 	disableNodes     = []string{"node6:12345"}
-	// )
-	//
-	// mockNetwork := new(MockNetworkOperations)
-	// cl := &Client{
-	// 	hr:      consistenthash.NewHashRing(),
-	// 	timeout: -1,
-	// 	nw: &network{
-	// 		dial:       mockNetwork.Dial,
-	// 		lookupHost: mockNetwork.LookupHost,
-	// 	},
-	// 	cfg: &config{
-	// 		Servers: currentNodes,
-	// 	},
-	// }
-	//
-	// mockNetwork.On("LookupHost", cl.cfg.HeadlessServiceAddress).Return(currentNodes, nil)
-	// mockNetwork.On("Dial", addr.Network(), addr.String()).Return(nil, expectedErr)
-	//
-	// for _, node := range currentNodes {
-	// 	cl.hr.Add(node)
-	// }
-	// cl.deadNodes = make(map[string]struct{})
-	// for _, node := range alreadyDeadNodes {
-	// 	cl.deadNodes[node] = struct{}{}
-	// }
-	// for _, node := range disableNodes {
-	// 	cl.deadNodes[node] = struct{}{}
-	// }
-	//
-	// cl.checkNodesHealth()
-	//
-	// assert.Equal(t, 3, len(cl.hr.GetAllNodes()))
-	// assert.Equal(t, 2, len(cl.deadNodes))
+	var (
+		mockNetworkErr = new(MockNetworkOperations)
+
+		expectedErr = errors.New("mocked dial error")
+	)
+	cl := &Client{
+		nw: &network{
+			dial:       mockNetworkErr.Dial,
+			lookupHost: mockNetworkErr.LookupHost,
+		},
+		cfg: &config{
+			HeadlessServiceAddress: "example.com",
+		},
+	}
+
+	mockNetworkErr.On("LookupHost", cl.cfg.HeadlessServiceAddress).Return(nil, expectedErr)
+	mockNetworkErr.On("Dial", mock.Anything, mock.Anything).Return(&FakeConn{}, nil)
+
+	cl.checkNodesHealth()
+
+	mockNetworkErr.AssertNotCalled(t, "Dial")
+	mockNetworkErr.AssertNumberOfCalls(t, "LookupHost", 1)
+
+	var (
+		currentNodes     = []string{"127.0.0.1:12345", "127.0.0.2:12345", "127.0.0.3:12345", "127.0.0.4:12345", "127.0.0.5:12345"}
+		alreadyDeadNodes = []string{"127.0.0.4:12345", "127.0.0.5:12345"}
+		disableNodes     = []string{"127.0.0.6:12345"}
+
+		mockNetwork = new(MockNetworkOperations)
+	)
+
+	cl = &Client{
+		hr:      consistenthash.NewHashRing(),
+		timeout: -1,
+		nw: &network{
+			dial:       mockNetwork.Dial,
+			lookupHost: mockNetwork.LookupHost,
+		},
+		cfg: &config{
+			Servers: currentNodes,
+		},
+	}
+
+	mockNetwork.On("Dial", "tcp", "127.0.0.2:12345").Return(nil, expectedErr).Once()
+	mockNetwork.On("Dial", "tcp", "127.0.0.4:12345").Return(nil, expectedErr).Once()
+	mockNetwork.On("Dial", mock.Anything, mock.Anything).Return(&FakeConn{}, nil)
+
+	for _, node := range currentNodes {
+		addr, _ := utils.AddrRepr(node)
+		cl.hr.Add(addr)
+	}
+	cl.deadNodes = make(map[string]struct{})
+	for _, node := range alreadyDeadNodes {
+		cl.deadNodes[node] = struct{}{}
+	}
+	for _, node := range disableNodes {
+		cl.deadNodes[node] = struct{}{}
+	}
+
+	cl.checkNodesHealth()
+
+	assert.Equal(t, 3, len(cl.hr.GetAllNodes()))
+	assert.Equal(t, 2, len(cl.deadNodes))
+}
+
+func Test_rebuildNodes(t *testing.T) {
+	var (
+		mockNetworkErr = new(MockNetworkOperations)
+
+		expectedErr = errors.New("mocked dial error")
+	)
+	cl := &Client{
+		nw: &network{
+			dial:       mockNetworkErr.Dial,
+			lookupHost: mockNetworkErr.LookupHost,
+		},
+		cfg: &config{
+			HeadlessServiceAddress: "example.com",
+		},
+	}
+
+	mockNetworkErr.On("LookupHost", cl.cfg.HeadlessServiceAddress).Return(nil, expectedErr)
+	mockNetworkErr.On("Dial", mock.Anything, mock.Anything).Return(&FakeConn{}, nil)
+
+	cl.rebuildNodes()
+
+	mockNetworkErr.AssertNotCalled(t, "Dial")
+	mockNetworkErr.AssertNumberOfCalls(t, "LookupHost", 1)
+
+	var (
+		currentNodes        = []string{"127.0.0.1:12345", "127.0.0.2:12345", "127.0.0.3:12345", "127.0.0.4:12345", "127.0.0.5:12345"}
+		alreadyDeadNodes    = []string{"127.0.0.4:12345", "127.0.0.2:12345"}
+		expectedNodesInRing = []string{"127.0.0.1:12345", "127.0.0.3:12345", "127.0.0.5:12345"}
+
+		mockNetwork = new(MockNetworkOperations)
+	)
+	cl = &Client{
+		ctx: context.TODO(),
+		nw: &network{
+			dial:       mockNetwork.Dial,
+			lookupHost: mockNetwork.LookupHost,
+		},
+		cfg: &config{
+			Servers: currentNodes,
+		},
+		timeout:      -1,
+		maxIdleConns: 1,
+		hr:           consistenthash.NewHashRing(),
+	}
+
+	mockNetwork.On("LookupHost", cl.cfg.Servers).Return(currentNodes, nil)
+	mockNetwork.On("Dial", mock.Anything, mock.Anything).Return(&FakeConn{}, nil)
+
+	cl.deadNodes = make(map[string]struct{})
+	for _, node := range alreadyDeadNodes {
+		cl.deadNodes[node] = struct{}{}
+	}
+	// len(currentNodes)-1 simulates the absence of one node in the hash ring.
+	for i := 0; i < len(currentNodes)-1; i++ {
+		addr, _ := utils.AddrRepr(currentNodes[i])
+		cl.hr.Add(addr)
+	}
+
+	// simulating the borrowing of a connection from the connection pool by taking a connection and putting it back.
+	// for test CloseAvailableConnsInAllShardPools
+	for i := 0; i < len(currentNodes)-1; i++ {
+		node, ok := cl.hr.Get(currentNodes[i])
+		require.Truef(t, ok, "Not found node (%s) in hash ring", currentNodes[i])
+		cn, err := cl.getConnForNode(node)
+		require.Nil(t, err, "getConnForNode try get conn")
+		cn.condRelease(new(error))
+	}
+
+	cl.rebuildNodes()
+
+	assert.Equal(t, 3, cl.hr.GetNodesCount())
+
+	var actualNodesInRing []string
+	for _, node := range cl.hr.GetAllNodes() {
+		actualNodesInRing = append(actualNodesInRing, node.(net.Addr).String())
+	}
+
+	slices.Sort(actualNodesInRing)
+	slices.Sort(expectedNodesInRing)
+	assert.Equal(t, expectedNodesInRing, actualNodesInRing)
+
+	for _, pool := range cl.freeConns {
+		assert.Equal(t, 0, pool.Len())
+	}
 }
 
 type MockNetworkOperations struct {
